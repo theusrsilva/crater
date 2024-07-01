8f07ff0295c4926610ac4cb5f9e419fe02a61c7e
unknown <lpmenezes@id.uff.br>
2024-06-30 22:23:49 -0300

    Enviando Testes Unitários CustomFieldValueTest

--- tests/Unit/CustomFieldValueTest.php
+++ tests/Unit/CustomFieldValueTest.php
@@ -19,3 +19,48 @@ test('custom field value belongs to custom field', function () {
 
     $this->assertTrue($fieldValue->customField()->exists());
 });
+
+
+test('set time answer attribute', function () {
+    $fieldValue = CustomFieldValue::factory()->create();
+
+    $fieldValue->time_answer = '15:30:00';
+    $this->assertEquals('15:30:00', $fieldValue->time_answer);
+
+    $fieldValue->time_answer = null;
+    $this->assertNull($fieldValue->time_answer);
+});
+
+
+test('get default answer attribute', function () {
+    $fieldValue = CustomFieldValue::factory()->create([
+        'type' => 'Input',
+        "string_answer" => "teste"// 
+    ]);
+    $this->assertEquals("teste", $fieldValue->defaultAnswer);
+});
+
+
+test('custom field value morphs to valuable', function () {
+    // Crie um modelo válido, por exemplo, Invoice
+    $invoice = \Crater\Models\Invoice::factory()->create();
+
+    // Crie um CustomFieldValue com os campos necessários
+    $fieldValue = \Crater\Models\CustomFieldValue::factory()->create([
+        'custom_field_valuable_type' => \Crater\Models\Invoice::class,
+        'custom_field_valuable_id' => $invoice->id,
+    ]);
+
+    // Verifique se o relacionamento polimórfico está funcionando corretamente
+    $this->assertInstanceOf(\Crater\Models\Invoice::class, $fieldValue->customFieldValuable);
+});
+
+
+
+test('custom field value has guarded and dates properties', function () {
+    $fieldValue = new CustomFieldValue();
+
+    $this->assertContains('id', $fieldValue->getGuarded());
+    $this->assertContains('date_answer', $fieldValue->getDates());
+    $this->assertContains('date_time_answer', $fieldValue->getDates());
+});
